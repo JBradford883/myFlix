9f1ede78deea1269ac8f25126a4395e66ddf85b7
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-05-03 14:14:15 -0400

    Removed internal database list

--- index.js
+++ index.js
@@ -15,50 +15,6 @@ const app = express();
 
 app.use(bodyParser.json());
 
-// List of movies
-let movies = [
-  {
-    title: 'The Shawshank Redemption',
-    director:'Frank Darabont'
-  },
-  {
-    title: 'The Godfather',
-    director: 'Francis Ford Coppola'
-  },
-  {
-    title: 'The Godfather Part:II',
-    director: 'Francis Ford Coppola'
-  },
-  {
-    title: 'The Dark Knight',
-    director: 'Christopher Nolan'
-  },
-  {
-    title: '12 Angry Men',
-    director: 'Sidney Lumet'
-  },
-  {
-    title: 'Schindler\'s List',
-    director: 'Steven Spielberg'
-  },
-  {
-    title: 'The Lord of the Rings: The Return of the King',
-    director: 'Peter Jackson'
-  },
-  {
-    title: 'Pulp Fiction',
-    director: 'Quentin Tarantino'
-  },
-  {
-    title: 'The Good, the Bad and the Ugly',
-    director: 'Sergio Leono'
-  },
-  {
-    title: 'The Lord of the Rings: The Fellowship of the Ring',
-    director: 'Peter Jackson'
-  },
-];
-
 // Return Documentation.html
 app.use(express.static('public'));
 
