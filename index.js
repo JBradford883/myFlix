7750e07bc68e7711e790fd7df1000909fa9a594c
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-05-04 00:04:38 -0400

    Added logic allowing users to deregister their profile

--- index.js
+++ index.js
@@ -161,9 +161,20 @@ app.delete('/users/:Username/Movies/:MovieID', (req, res) => {
   });
 });
 
-// Allow existing users to deregister (showing only a text that a user email has been removed)
-app.delete('/users/:username', (req, res) => {
-  res.send('Returns a DELETE request showing that the user has been removed')
+// Allow existing users to deregister
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+  .then((user) => {
+    if (!user) {
+      res.status(400).send(req.params.Username + ' was not found');
+    } else {
+      res.status(200).send(req.params.Username + ' account was removed.');
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
 });
 
 // GET requests
