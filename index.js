50a9fd2896686693722d88e5e2630194d7adcfb9
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-05-06 21:08:46 -0400

    Added express-validator

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 
+const { check, validationResult } = require('express-validator');
 
 mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
