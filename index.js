5d3df03887860ed9fb155aa067f59bf79b74fa47
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-05-07 14:16:37 -0400

    Added validation logic to important fields when users update their info

--- index.js
+++ index.js
@@ -136,15 +136,21 @@ app.post('/users',
 });
 
 // Get a user by username
-app.get('/users/:Username', passport.authenticate('jwt', {session: false }), (req, res) => {
-  Users.findOne({ Username: req.params.Username })
-    .then((user) => {
-      res.json(user);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
+app.get('/users/:Username', passport.authenticate('jwt', {session: false }),
+  [
+    check('Username', 'Username is required').isLength({min: 6}),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ], (req, res) => {
+    Users.findOne({ Username: req.params.Username })
+      .then((user) => {
+        res.json(user);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
 });
 
 // Allows users to update their user info by username
