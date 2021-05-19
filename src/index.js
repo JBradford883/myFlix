f401c34c72189b919076b118f6d6420d3d3cb738
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-05-19 12:12:48 -0400

    Removed authentication for React app build

--- src/index.js
+++ src/index.js
@@ -48,7 +48,7 @@ app.use(express.static('public'));
 Returns a list of all movies to the users.
 Protected route.
 */
-app.get('/movies', /* passport.authenticate('jwt', { session: false }), */(req, res) => {
+app.get('/movies', (req, res) => {
   Movies.find().then((movies) => {
     res.status(201).json(movies);
   })
