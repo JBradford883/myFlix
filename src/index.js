6748174afc6ac5005ad8a4cb40d38af9dee083b9
Justin Bradford <78094730+JBradford883@users.noreply.github.com>
2021-06-09 12:50:12 -0400

    Testing

--- src/index.js
+++ src/index.js
@@ -48,14 +48,13 @@ app.use(express.static('public'));
 Returns a list of all movies to the users.
 Protected route.
 */
-app.get("/movies", function (req, res) {
-  Movies.find()
-    .then(function (movies) {
-      res.status(201).json(movies);
-    })
-    .catch(function (error) {
-      console.error(error);
-      res.status(500).send("Error: " + error);
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Movies.find().then((movies) => {
+    res.status(201).json(movies);
+  })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
     });
 });
 
